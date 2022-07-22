6fb700e3b0606f2cf11023ecf4079536f2d90046
Mironshoh <mironshoh1545@gmail.com>
2022-07-22 15:37:29 +0500

    .\Index01.py - .Index10.py

--- Index05.py
+++ Index05.py
@@ -1,3 +1,8 @@
+def nu(s):
+    if s == '1' or s == '2' or s == '3' or s == '4' or s == '5' or s == '6' or s == '7' or s == '8' or s == '9' or s =='0':
+        return 1
+    return 0
+
 def main(s):
     """
     Given a variable s string of length five. Determine the number of digits involved in this variable.
@@ -6,4 +11,5 @@ def main(s):
     Returns:
         int: answer
     """
-    return
+    answer = nu(s[0]) + nu(s[1]) + nu(s[2]) + nu(s[3]) + nu(s[4])
+    return answer
